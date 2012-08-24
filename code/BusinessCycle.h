d7f86df9633294f1f9ff6e67f7546eeb6733860f
Chet Parrott <chet.parrott@aclecta.com>
2012-08-24 18:54:30 -0500

    compiles again

--- code/BusinessCycle.h
+++ code/BusinessCycle.h
@@ -4,9 +4,9 @@
 
 class BusinessCycle{
       private:
-              vector<Firm*> firmindex;
-              vector<Individual*> unemployed;
-              product productID;
+              std::vector<Firm*> firmindex;
+              std::vector<Individual*> unemployed;
+              Product productID;
       public:
              BusinessCycle();
              void Cycle();
