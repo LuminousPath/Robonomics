53e8e4461a46769bf915de427297209db14f29cd
Chet Parrott <chet.parrott@aclecta.com>
2012-08-28 15:01:38 -0500

    I further commit my changes

--- code/Simulation.cpp
+++ code/Simulation.cpp
@@ -2,7 +2,7 @@
 #include "time.h"
 
 Simulation::Simulation(int x){
-<<<<<<< HEAD
+
     /*
 	//testing basic Hamming Distance function
 	gType p1 = 127, p2 = 128;
@@ -17,14 +17,5 @@ Simulation::Simulation(int x){
 	
 	// this should be 1
 	std::cout<< " " <<getHammingDistance(p1,p2);
-=======
-     srand ( time(NULL) );
-     int k = 0;
-     //My name is chet, and I approve this comment.
-     //LOL ARE YOU KIDDING ME THIS IS THE GREATEST PROTOTYPE EVER
-     for(int i = 0; i < x; i++) printCrap(k++,7);
-}
->>>>>>> 2cf58d5f7664f776fbafc7ebac26e220c44028b4
-
 	*/
 }
